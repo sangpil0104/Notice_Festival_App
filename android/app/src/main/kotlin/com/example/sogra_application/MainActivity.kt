e543106824e64466cec15fcd4ca70941ff23664b
sangpil0104 <psp0104@naver.com>
2024-05-21 18:00:47 +0900

    test

---
+++ android/app/src/main/kotlin/com/example/sogra_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sogra_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/MyHomePage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('app'),
+      ),
+      body: const Column(
+        children: [
+          Text('hello'),
+        ],
+      ),
+    );
+  }
+}
