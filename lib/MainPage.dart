1f3ab176ee9105140f3fbb48671d8b012206bb53
ddubi0521 <qwert0483@naver.com>
2024-05-25 21:32:13 +0900

    Updated files

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -32,7 +32,7 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard App'),
+        title: Text('App'),
       ),
       body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
