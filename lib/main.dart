d7f95dd0359d1ecf8654cf482f60cf0be6876457
sangpil0104 <psp0104@naver.com>
2024-05-26 03:12:35 +0900

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'MainPage.dart';
+import 'FestivalListPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       home: MainPage(),
     );
   }
 }
+
+
 
